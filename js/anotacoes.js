e4bbf9ec704778f194a6fc8eb9bff91eb2c3219f
Lucas Silva <lucasvps@gmail.com>
2023-03-28 12:10:53 -0300

    atualizado edicao e exclusao de anotacoes,

--- js/anotacoes.js
+++ js/anotacoes.js
@@ -1,6 +1,3 @@
-// PAGINA CADASTRO ANOTACOES
-
-
 // VERIFICAR SE USUARIO ESTA LOGADO
 
 if (localStorage.getItem('usuarioAtivo') === null) {
@@ -8,81 +5,79 @@ if (localStorage.getItem('usuarioAtivo') === null) {
     window.location.href = "./index.html";
 }
 
-
-// RECEBER AS ANOTACOES
-
+//
 
 
-let listaAnotacoes = JSON.parse(localStorage.getItem('listaAnotacoes')) ?? [];
+let idAtualizar = -1
+let listaAnotacao = JSON.parse(localStorage.getItem('anotacoes')) ?? [];
 
-document.addEventListener('DOMContentLoaded', () => {
-    listaAnotacoes.forEach((anotacao) => addAnotacao(anotacao))
-})
+const formCadastro = document.getElementById('form-cadastro')
+const formAtualizar = document.getElementById('form-atualizar')
 
-
-
-
-const formAnotacoes = document.getElementById('formAnotacoes');
-
-const modalCadastro = new bootstrap.Modal('#modal-cadastro')
-const modalApagar = new bootstrap.Modal('#modal-apagar')
-
-
+const nameUpdate = document.getElementById('name-update')
+const descrevaUpdate = document.getElementById('descreva-update')
+const dataUpdate = document.getElementById('data-update')
 
 
 const row = document.getElementById('list-anotacoes')
+const containerNotificacao = document.getElementById('container-notificacao')
 
 
 
-// FUNCOES DO MODAL
+// MANIPULACAO DO BOOTSTRAP MODAL COM O JS
+const modalCadastro = new bootstrap.Modal('#modal-cadastro')
+const modalApagar = new bootstrap.Modal('#modal-apagar')
+const modalAtualizar = new bootstrap.Modal('#modal-atualizar')
 
-formAnotacoes.addEventListener('submit', (ev) => {
-    ev.preventDefault();
-
-
-    if (!formAnotacoes.checkValidity()) {
-        formAnotacoes.classList.add('was-validated');
-        return
-    }
-
-    const nameIdea = document.getElementById('nameIdea').value;
-    const descriptionIdea = document.getElementById('descriptionIdea').value;
-    const dataIdea = document.getElementById('dataIdea').value;
-
-    // const exist = listUsers.some((user) => user.userEmail === userEmail);
-
-
-
-    const newIdea = {
-        id: new Date().getTime(),
-        nameIdea,
-        descriptionIdea,
-        dataIdea,
-    }
-
-    listaAnotacoes.push(newIdea)
-    console.log(listaAnotacoes);
-    alert('Anotação Criada com sucesso!')
-    localStorage.setItem('listaAnotacoes', JSON.stringify(listaAnotacoes));
-    formAnotacoes.reset();
-    addAnotacao(newIdea);
-    modalCadastro.hide()
-    // formNewUser.classList.remove('was-validated');
-
+document.addEventListener('DOMContentLoaded', () => {
+    listaAnotacao.forEach((anotacoes) => addAnotacao(anotacoes))
 })
 
 
-// FUNCAO PARA ADICIONAR A ANOTACAO NA TELA
-
-function addAnotacao(anotacao) {
+// RECEBER AS INFORAMAÇÕES PREENCHIDAS PELO USUARIO 
 
 
-    const { id, nameIdea, descriptionIdea, dataIdea } = anotacao;
+formCadastro.addEventListener('submit', (ev) => {
+    ev.preventDefault()
+
+
+    if (!formCadastro.checkValidity()) {
+        formCadastro.classList.add('was-validated')
+        return
+    }
+
+    const name = document.getElementById('name').value
+    const descreva = document.getElementById('descreva').value
+    const data = document.getElementById('data').value
+
+
+    const novaAnotacao = {
+        id: new Date().getTime(),
+        name,
+        descreva,
+        data
+    }
+
+
+    listaAnotacao.push(novaAnotacao)
+    localStorage.setItem('anotacoes', JSON.stringify(listaAnotacao))
+    formCadastro.reset()
+    addAnotacao(novaAnotacao)
+    modalCadastro.hide()
+    formCadastro.classList.remove('was-validated')
+    showAlert('success', 'Anotação cadastrada!')
+})
+
+
+// ADICIONAR A ANOTAÇÃO NA TELA:
+
+
+function addAnotacao(anotacoes) {
+    const { id, name, descreva, data } = anotacoes;
 
     const col = document.createElement('div')
-    // col.classList.add(['col-12', 'col-sm-6', 'col-lg-4', 'col-xl-3'])  - esse salva sem apagar
     col.setAttribute('class', 'col-12 col-sm-6 col-lg-4 col-xl-3')
-    col.setAttribute('id', `${id}`)
+    col.setAttribute('id', `anotacao-${id}`)
 
     const card = document.createElement('div')
     card.setAttribute('class', 'card')
@@ -91,50 +86,147 @@ function addAnotacao(anotacao) {
     cardBody.setAttribute('class', 'card-body')
 
     const cardTitle = document.createElement('h5')
-    cardTitle.setAttribute('class', 'card-title')
-    cardTitle.innerHTML = nameIdea
+    cardTitle.setAttribute('class', 'card-title mb-4')
+    cardTitle.innerHTML = name
 
     const cardText = document.createElement('p')
-    cardText.setAttribute('class', 'card-text')
-    cardText.innerHTML = descriptionIdea
+    cardText.setAttribute('class', 'card-text mt-2')
+    cardText.innerHTML = descreva
 
-    const cardDate = document.createElement('p')
-    cardDate.setAttribute('class', 'card-text')
-    cardDate.innerHTML = dataIdea
+    const cardData = document.createElement('p')
+    cardData.setAttribute('class', 'text-body-secondary')
+    cardData.innerHTML = `Prazo para concluir: ${data}`
 
     const buttonEdit = document.createElement('button')
-    buttonEdit.setAttribute('class', 'btn btn-success')
+    buttonEdit.setAttribute('class', 'btn btn-success m-1')
     buttonEdit.addEventListener('click', () => {
         modalAtualizar.show()
-
-        // arrumar
-
+        nameUpdate.value = name
+        descrevaUpdate.value = descreva
+        dataUpdate.value = data
         idAtualizar = id
-
     })
     buttonEdit.innerHTML = `<i class="bi bi-pencil-square"></i>`
 
+
     const buttonDelete = document.createElement('button')
-    buttonDelete.setAttribute('class', 'btn btn-danger')
+    buttonDelete.setAttribute('class', 'btn btn-danger m-1')
     buttonDelete.addEventListener('click', () => {
         modalApagar.show()
-
-        // arrumar
-
+        const confirmar = document.getElementById('confirmar-exclusao')
+        confirmar.setAttribute('onclick', `apagar(${id})`)
     })
     buttonDelete.innerHTML = `<i class="bi bi-trash3"></i>`
 
-
     cardBody.appendChild(cardTitle)
     cardBody.appendChild(cardText)
-    cardBody.appendChild(cardDate)
+    cardBody.appendChild(cardData)
     cardBody.appendChild(buttonEdit)
     cardBody.appendChild(buttonDelete)
 
     card.appendChild(cardBody)
     col.appendChild(card)
 
-
     row.appendChild(col)
+}
+
+// ATUALIZAR AS ANOTAÇÕES
+
+formAtualizar.addEventListener('submit', (ev) => {
+    ev.preventDefault();
+
+
+    if (!formAtualizar.checkValidity()) {
+        formAtualizar.classList.add('was-validated')
+        return
+    }
+
+
+    const indiceUpdate = listaAnotacao.findIndex((anotacoes) => anotacoes.id === idAtualizar);
+    listaAnotacao[indiceUpdate].name = nameUpdate.value
+    listaAnotacao[indiceUpdate].descreva = descrevaUpdate.value
+    listaAnotacao[indiceUpdate].data = dataUpdate.value
+
+
+
+    localStorage.setItem('anotacoes', JSON.stringify(listaAnotacao))
+
+
+    const cardTitle = document.querySelector(`#anotacao-${idAtualizar} .card-title`);
+    cardTitle.innerHTML = nameUpdate.value
+
+    const cardText = document.querySelector(`#anotacao-${idAtualizar} .card-text`);
+    cardText.innerHTML = descrevaUpdate.value
+
+    const cardData = document.querySelector(`#anotacao-${idAtualizar} .text-body-secondary`);
+    cardData.innerHTML = dataUpdate.value
+
+
+
+    modalAtualizar.hide()
+    showAlert('success', 'Anotação atualizada!')
+    idAtualizar = -1
+    formAtualizar.classList.remove('was-validated')
+
+
+
+})
+
+
+// FUNÇÃO PARA APAGAR AS ANOTAÇÕES
+
+
+function apagar(idAnotacao) {
+
+    const indice = listaAnotacao.findIndex((anotacoes) => anotacoes.id === idAnotacao)
+    listaAnotacao.splice(indice, 1)
+
+    localStorage.setItem('anotacoes', JSON.stringify(listaAnotacao))
+
+    const col = document.getElementById(`anotacao-${idAnotacao}`)
+    col.remove()
+
+    modalApagar.hide()
+    showAlert('success', 'Anotação excluida!')
+}
+
+// FUNÇÃO PARA MOSTRAR OS ALERTAS
+
+function showAlert(modo, mensagem) {
+
+
+    const toast = document.createElement('div');
+    toast.setAttribute('role', 'alert')
+    toast.setAttribute('aria-live', 'assertive');
+    toast.setAttribute('aria-atomic', 'true');
+    toast.setAttribute('class', 'toast align-items-center border-0 show');
+    toast.classList.add(`text-bg-${modo}`);
+
+    const content = document.createElement('div');
+    content.setAttribute('class', 'd-flex');
+
+    const toastBody = document.createElement('div')
+    toastBody.setAttribute('class', 'toast-body')
+    toastBody.innerHTML = `${mensagem}`
+
+    const butttonDismiss = document.createElement('button')
+    butttonDismiss.setAttribute('type', 'button')
+    butttonDismiss.setAttribute('class', 'btn-close btn-close-white me-2 m-auto')
+    butttonDismiss.setAttribute('data-bs-dismiss', 'toast')
+    butttonDismiss.setAttribute('aria-label', 'Fechar notificação')
+
+    content.appendChild(toastBody)
+    content.appendChild(butttonDismiss)
+    toast.appendChild(content);
+
+    containerNotificacao.appendChild(toast)
+
+
+    setTimeout(() => {
+        containerNotificacao.children[0].remove()
+    }, 3000)
 
 }
+
+
+
